ee1a8085f98ef4627e565a43c52ff7f2dac0ebae
Imeah <imeahdornu@gmail.com>
2024-01-20 11:47:11 +0100

    added backend url

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: "https://falcon-net.onrender.com/api/v1",
   // withCredentials: true,
 });
 
